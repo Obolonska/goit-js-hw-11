1f32e2fa8ee58b07eaaa56483cca33da1e7b05e9
Obolonska <velentiy@gmail.com>
2025-05-14 12:26:56 +0300

    Initial commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(inputDelay.value);
+  const inputState = document.querySelector('input[name="state"]:checked');
+  const state = inputState.value;
+
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  myPromise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  inputDelay.value = '';
+});
